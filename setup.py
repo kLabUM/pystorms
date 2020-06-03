18a547cd8af0bb5bd02902f8f9f88765bd90bd39
Abhiram <abhiramm@umich.edu>
2020-06-02 22:45:19 -0400

    HOPE is bad thing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pystorms",
-    version="0.2.0",
+    version="0.1.7",
     description="Simulation Sandbox for the development and evaluation of stormwater control algorithms",
     author="Abhiram Mullapudi, Sara C. Troutman, Sara Rimer, Branko Kerkez",
     author_email="abhiramm@umich.edu, stroutm@umich.edu",
